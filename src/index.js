efea966649d46c2b9a11af6ade2a0260ffcd288f
Alexander Romanov <raibinger@gmail.com>
2023-06-30 21:06:00 +0300

    modified index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-
+import _ from 'lodash';
 export default str => str.split('').reverse().join('');
+
+// export const right1 = _.without();
+
+export const wrong1 = (coll = [], ...values) => {
+  const [result] = coll.reduce(([prevColl, vals], item) => {
+    const newVals = vals.filter((val) => val !== item);
+    const nextColl = newVals.length === vals.length ? [...prevColl, item] : prevColl;
+    return [nextColl, newVals];
+  }, [[], values]);
+
+  return result;
+};
+
+console.log(_.without([3, 8, 9, 8, 10], 8, 10));
+
+console.log(wrong1([3, 8, 9, 8, 10], 8, 10));
